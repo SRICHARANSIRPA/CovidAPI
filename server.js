1cf35d0d461edfe05a61c4308d63a2095412861b
Sri Charan Sirpa <Sri.Sirpa@odessainc.com>
2020-12-08 13:11:41 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
-
+app.get("/", (req, res) => {
+  return res.send("HELLO WORLD");
+});
 //Listining to PORT
 const port = process.env.COVID_PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
